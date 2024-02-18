4cd0e49dd22217fa95707c9511814564dd1c44c8
iroberth <igorroberth1992@gmail.com>
2024-02-18 15:30:47 -0300

    Criado novos casos de testes para validar o produto apresentado

--- src/test/java/login/LoginConexao.java
+++ src/test/java/login/LoginConexao.java
@@ -3,12 +3,8 @@ package login;
 import browser.Navegadores;
 import driver.Driver;
 import elementos.*;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runners.MethodSorters;
-
 import static configurl.ConfigUrl.*;
 import static metodos.Metodos.*;
 import static org.junit.Assert.assertTrue;
@@ -32,7 +28,7 @@ public class LoginConexao extends Driver {
     final static String msgFinal = "Thank you for your order!";
 
     @Test
-    public void t1_compracomsucesso() {
+    public void t1_CompraComSucesso() {
 
         escrever(Elementos.login, getUser());
         escrever(Elementos.senha, getSenha());
@@ -48,13 +44,13 @@ public class LoginConexao extends Driver {
         clicar(DadosPessoais.Continue);
         clicar(DadosPessoais.finish);
         validarItem(DadosPessoais.msgFinal, msgFinal);
-        System.out.println("\n===== Produto " + produto + " e mensagem " + msgFinal + " validado com sucesso =====");
+        System.out.println("\n===== Produto " + produto + " e mensagem " + msgFinal + " validadas com sucesso =====");
     }
 
     final static String msgErro = "Epic sadface: Sorry, this user has been locked out.";
 
     @Test
-    public void t2_usuariocombloqueio() {
+    public void t2_UsuarioComBloqueio() {
 
         escrever(Elementos.login, getUserBloq());
         escrever(Elementos.senha, getSenha());
@@ -63,10 +59,10 @@ public class LoginConexao extends Driver {
         System.out.println("======== Mensagem de erro validada com sucesso: " + msgErro + "=======");
     }
 
-    final static String userProblem = "Test.allTheThings() T-Shirt (Red)";
+    final static String userProblem = "Test.allTheThings() T-Shirt (Red) ";
 
     @Test
-    public void t3_usuariocomproblemadeinterface() {
+    public void t3_UsuarioComProblemaDeInterface() {
 
         escrever(Elementos.login, getUserProblem());
         escrever(Elementos.senha, getSenha());
@@ -76,10 +72,10 @@ public class LoginConexao extends Driver {
         System.out.println("======== Produto esperado validado com sucesso: " + userProblem + "=======");
     }
 
-    final static String msgsenhaIncorreta = "Epic sadface: Username and password do not match any user in this service ";
+    final static String msgsenhaIncorreta = "Epic sadface: Username and password do not match any user in this service";
 
     @Test
-    public void t4_tentativadelogincomsenhaincorreta() {
+    public void t4_TentativaDeLoginComSenhaIncorreta() {
 
         escrever(Elementos.login, getUser());
         escrever(Elementos.senha, getSenhaIncorreta());
@@ -87,11 +83,12 @@ public class LoginConexao extends Driver {
         validarItem(Elementos.msgSenhaIncorreta, msgsenhaIncorreta);
         System.out.println("======== Mensagem de erro validada com sucesso: " + msgsenhaIncorreta + "========");
     }
+
     @Test
-    public void t5_validacaodeperformace() {
+    public void t5_ValidacaoDePerformace() {
 
         long startTime = System.currentTimeMillis();
-        escrever(Elementos.login, getPerformance_Glitch_User());
+        escrever(Elementos.login, getPerformanceGlitchUser());
         escrever(Elementos.senha, getSenha());
         clicar(Elementos.button);
 
@@ -101,4 +98,26 @@ public class LoginConexao extends Driver {
         System.out.println("O login demorou: " + durationInSeconds + " segundos.");
         assertTrue("O login demorou mais do que o esperado", durationInSeconds < 6);
     }
+
+    private static final String ImagemSelecionada = "https://www.saucedemo.com/static/media/sl-404.168b1cce.jpg";
+    private static final String ImagemApresentada = "https://www.saucedemo.com/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg";
+
+    @Test
+    public void t6_ValidarDiferencaEntreImagensDeProdutos() {
+
+        escrever(Elementos.login, getVisual_User());
+        escrever(Elementos.senha, getSenha());
+        clicar(Elementos.button);
+
+        String urlPrimeiraImagem = obterUrlDaImagem(ImagemSelecionada);
+        String urlSegundaImagem = obterUrlDaImagem(ImagemApresentada);
+        Assert.assertNotEquals(urlPrimeiraImagem, urlSegundaImagem);
+        System.out.println("As URLs das imagens dos produtos não deveriam ser iguais.");
+    }
+
+    private String obterUrlDaImagem(String urlImagem) {
+        driver.get(urlImagem);
+        return driver.getCurrentUrl();
+    }
+
 }
