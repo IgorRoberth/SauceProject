048b45277823e266850015373d2001ee5213af92
iroberth <igorroberth1992@gmail.com>
2024-02-18 15:34:28 -0300

    Apagado um elemento no UserBloq que não estava sendo utilizado e removido dois elementos, que não estava usando na page Elementos

--- src/test/java/elementos/Elementos.java
+++ src/test/java/elementos/Elementos.java
@@ -6,4 +6,6 @@ public class Elementos {
         public static By senha = By.cssSelector("#password");
         public static By button = By.cssSelector("#login-button");
         public static By msgSenhaIncorreta = By.xpath("//*[text()='Epic sadface: Username and password do not match any user in this service']");
+        public static By imagemSelecionada = By.cssSelector("#item_4_img_link > img");
+        public static By imagemApresentada = By.cssSelector("#inventory_item_container > div > div > div.inventory_details_img_container > img");
 }
--- src/test/java/elementos/UserBloq.java
+++ src/test/java/elementos/UserBloq.java
@@ -3,8 +3,6 @@ package elementos;
 import org.openqa.selenium.By;
 
 public class UserBloq {
-
-    public static By userBloq = By.cssSelector("input[id=user-name]");
     public static By validarErro = By.xpath("//*[text()='Epic sadface: Sorry, this user has been locked out.']");
 
 }
