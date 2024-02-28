5b70d2b2e306e4ac71ff20febf79257aab1ccab1
iroberth <igorroberth1992@gmail.com>
2024-02-28 14:59:03 -0300

    Metodo para tirar print e salvar no pdf

--- src/test/java/pdfscreenshot/take/TakeScreenshot.java
+++ src/test/java/pdfscreenshot/take/TakeScreenshot.java
@@ -6,7 +6,6 @@ import driver.Driver;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
