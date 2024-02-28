2c4061da6c992e981d67908d7aa1d0ec2c62b578
iroberth <igorroberth1992@gmail.com>
2024-02-28 14:56:09 -0300

    Adicionado 2 novos elementos

--- src/test/java/elementos/VisualUser.java
+++ src/test/java/elementos/VisualUser.java
@@ -1,2 +1,12 @@
-package elementos;public class VisualUser {
+package elementos;
+
+import org.openqa.selenium.By;
+
+public class VisualUser {
+
+    public static By produtoSelecionado = By.cssSelector("#add-to-cart-sauce-labs-backpack");
+
+    public static By carrinhoSele = By.cssSelector("#shopping_cart_container");
+
+
 }
